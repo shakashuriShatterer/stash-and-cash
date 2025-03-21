c9a75bdadb2a31b6c2845cd6f9a4a6d8aab6fe00
shakashuriShatterer <oeagle@protonmail.com>
2025-03-21 19:57:55 +0000

    adding plant pots

--- entity.h
+++ entity.h
@@ -1,95 +1,99 @@
 #pragma once
 
+#include "raylib.h"
 #include <chrono>
 #include <string>
-#include "raylib.h"
 
-struct IdManager
-{
-    static int nextId;
-    static int idGenerator();
+struct IdManager {
+  static int nextId;
+  static int idGenerator();
 };
 
-class Entity
-{
+class Entity {
 private:
-    Texture texture;
-    Vector2 position, inventoryPosition;
-    const std::string name;
-    const int id;
-    bool isBeingDragged;
-    bool isDraggable;
-    bool isInteractionMenuShowing;
+  Texture texture;
+  Vector2 position, inventoryPosition;
+  const std::string name;
+  const int id;
+  bool isBeingDragged;
+  bool isDraggable;
+  bool isInteractionMenuShowing;
+
 public:
-    Entity(const std::string& );
-    // Setters
-    virtual void update();
-    void setTexture(const Texture&);
-    void setTextureSize(const int, const int);
-    void setPosition(const Vector2&);
-    void setPositionX(const float);
-    void setPositionY(const float);
-    void setInventoryPosition(const Vector2&);
-    void setInventoryPositionX(const float );
-    void setInventoryPositionY(const float );
-    void setIsBeingDragged(bool);
-    void setIsDraggable(bool);
-    void setIsInteractionMenuShowing(bool);
-    float scale = 1;
-    
+  Entity(const std::string &);
+  // Setters
+  virtual void update();
+  void setTexture(const Texture &);
+  void setTextureSize(const int, const int);
+  void setPosition(const Vector2 &);
+  void setPositionX(const float);
+  void setPositionY(const float);
+  void setInventoryPosition(const Vector2 &);
+  void setInventoryPositionX(const float);
+  void setInventoryPositionY(const float);
+  void setIsBeingDragged(bool);
+  void setIsDraggable(bool);
+  void setIsInteractionMenuShowing(bool);
+  float scale = 1;
 
+  // Getters
+  const Texture &getTexture() const;
+  int getTextureWidth() const;
+  int getTextureHeight() const;
+  const Vector2 &getPosition() const;
+  float getPositionX() const;
+  float getPositionY() const;
+  const Vector2 &getInventoryPosition() const;
+  float getInventoryPositionX() const;
+  float getInventoryPositionY() const;
+  int getId() const;
+  const std::string &getName() const;
+  bool getIsBeingDragged() const;
+  bool getIsDraggable() const;
+  bool getIsInteractionMenuShowing() const;
 
-    // Getters
-    const Texture& getTexture() const;
-    int getTextureWidth() const;
-    int getTextureHeight() const;
-    const Vector2& getPosition() const;
-    float getPositionX() const;
-    float getPositionY() const;
-    const Vector2& getInventoryPosition() const;
-    float getInventoryPositionX() const;
-    float getInventoryPositionY() const;
-    int getId() const;
-    const std::string& getName() const;
-    bool getIsBeingDragged() const;
-    bool getIsDraggable() const;
-    bool getIsInteractionMenuShowing() const;
-
-    virtual void interactionMenu();
+  virtual void interactionMenu();
 };
 
-class Computer : public Entity
-{
+class Computer : public Entity {
 private:
-    Texture desktopTexture;
-public:
+  Texture desktopTexture;
 
-    Computer();
-    void interactionMenu() override;
-    //setters
-    void setDesktopTexture(const Texture&);
-    //getters
-    const Texture& getDesktopTexture() const;
+public:
+  Computer();
+  void interactionMenu() override;
+  // setters
+  void setDesktopTexture(const Texture &);
+  // getters
+  const Texture &getDesktopTexture() const;
 };
 
-class Ganja : public Entity
-{
+class PlantPot : public Entity {
 private:
-    std::chrono::steady_clock::time_point timePlanted, timePlantedNew;
-    std::chrono::duration<double> timePlantedElapsed;
-    int growthCount;
+  int test;
+
 public:
-    Ganja();
-    void update() override { grow(); }
-    void interactionMenu() override;
-    void grow();
-    //setters
-    void setTimePlanted(const std::chrono::steady_clock&);
-    void setTimeNew(const std::chrono::steady_clock&);
+  PlantPot();
+  void interactionMenu() override;
+};
 
-    //getters
-    const std::chrono::steady_clock::time_point getTimePlanted() const;
-    const std::chrono::steady_clock::time_point getTimePlantedNew() const ;
-    const std::chrono::duration<double> getTimeElapsed() const;
+class Ganja : public Entity {
+private:
+  std::chrono::steady_clock::time_point timePlanted, timePlantedNew;
+  std::chrono::duration<double> timePlantedElapsed;
+  int growthCount;
 
+public:
+  Ganja();
+  void update() override { grow(); }
+  void interactionMenu() override;
+  void grow();
+  // setters
+  void setTimePlanted(const std::chrono::steady_clock &);
+  void setTimeNew(const std::chrono::steady_clock &);
+
+  // getters
+  const std::chrono::steady_clock::time_point getTimePlanted() const;
+  const std::chrono::steady_clock::time_point getTimePlantedNew() const;
+  const std::chrono::duration<double> getTimeElapsed() const;
 };
--- player.cpp
+++ player.cpp
@@ -1,18 +1,15 @@
+#include "player.h"
+#include "entity.h"
 #include <algorithm>
 #include <iostream>
 #include <math.h>
-#include "player.h"
-#include "gameManager.h"
 
-// gayboy hahahah smelly shit 
-Player::Player()
-	:Entity("player")
-{
-	setTexture(LoadTexture("sprite-0001.png"));
-	setTextureSize(100, 100);
-	setPosition({ 100,20 });
-	movementSpeed = 5;
-	canMove = true;
+Player::Player() : Entity("player") {
+  setTexture(LoadTexture("sprite-0001.png"));
+  setTextureSize(100, 100);
+  setPosition({100, 20});
+  movementSpeed = 5;
+  canMove = true;
 }
 
 void Player::setCanMove(bool move) { canMove = move; }
@@ -20,147 +17,131 @@ void Player::setCanMove(bool move) { canMove = move; }
 bool Player::getCanMove() const { return canMove; }
 int Player::getMovementSpeed() const { return movementSpeed; }
 
+void Player::move() {
+  if (canMove && !inventory.isInventoryShowing) {
+    Vector2 direction = {0, 0};
+    if (IsKeyDown(KEY_W))
+      direction.y -= 1;
+    if (IsKeyDown(KEY_S))
+      direction.y += 1;
+    if (IsKeyDown(KEY_A))
+      direction.x -= 1;
+    if (IsKeyDown(KEY_D))
+      direction.x += 1;
 
-void Player::move()
-{	
-	if (canMove && !inventory.isInventoryShowing)
-	{
-		Vector2 direction = { 0,0 };
-		if (IsKeyDown(KEY_W))
-			direction.y -= 1;
-		if (IsKeyDown(KEY_S))
-			direction.y += 1;
-		if (IsKeyDown(KEY_A))
-			direction.x -= 1;
-		if (IsKeyDown(KEY_D))
-			direction.x += 1;
+    // normalising vector to fix diagonal speed
+    if (direction.x != 0 || direction.y != 0) {
+      float magnitude =
+          sqrt(direction.y * direction.y + direction.x * direction.x);
 
-		// normalising vector to fix diagonal speed
-		if (direction.x != 0 || direction.y != 0)
-		{
-			float magnitude = sqrt(direction.y * direction.y + direction.x * direction.x);
+      direction.x /= magnitude;
+      direction.y /= magnitude;
+    }
 
-			direction.x /= magnitude ;
-			direction.y /= magnitude ;
-		}
-
-		setPositionX({ getPositionX() + direction.x * movementSpeed });
-		setPositionY({ getPositionY() + direction.y * movementSpeed });
-
-	}
+    setPositionX({getPositionX() + direction.x * movementSpeed});
+    setPositionY({getPositionY() + direction.y * movementSpeed});
+  }
 }
 
-void Player::interact()
-{
-		if (IsKeyPressed(KEY_E))
-			DrawRectangle(0, 0, 100, 100, YELLOW);
-
+void Player::interact() {
+  if (IsKeyPressed(KEY_E))
+    DrawRectangle(0, 0, 100, 100, YELLOW);
 }
 
-void Player::buyWeed()
-{
-	static bool keyPessedOnce = false;
+void Player::buyWeed() {
+  static bool keyPessedOnce = false;
 
-	if (IsKeyPressed(KEY_B) && !keyPessedOnce)
-	{
-		std::shared_ptr<Ganja> newWeed = std::make_shared<Ganja>();
-		inventory.inventoryVector.push_back(newWeed);
-		std::cout << "weedId: " << newWeed->getId() << "\n";
-		keyPessedOnce = true;
-
-	}
-	if (IsKeyReleased(KEY_B))
-		keyPessedOnce = false;
+  if (IsKeyPressed(KEY_B) && !keyPessedOnce) {
+    std::shared_ptr<PlantPot> newPot = std::make_shared<PlantPot>();
+    inventory.inventoryVector.push_back(newPot);
+    std::cout << "potId: " << newPot->getId() << "\n";
+    keyPessedOnce = true;
+  }
+  if (IsKeyReleased(KEY_B))
+    keyPessedOnce = false;
 }
 
+void Player::Inventory::drawInventoryMenu() {
+  const float spacing = 50;
+  const float startX = 10.f;
+  float startY = 10.f;
+  const int maxItemsPerRow = (GetRenderWidth() - startX) / spacing;
 
-void Player::Inventory::drawInventoryMenu()
-{
-	const float spacing = 50; 
-	const float startX = 10.f;
-	float startY = 10.f; 
-	const int maxItemsPerRow = (GetRenderWidth() - startX) / spacing; 
+  if (IsKeyPressed(KEY_TAB))
+    isInventoryShowing = true;
+  if (IsKeyPressed(KEY_Q))
+    isInventoryShowing = false;
 
-	if (IsKeyPressed(KEY_TAB))
-		isInventoryShowing = true;
-	if (IsKeyPressed(KEY_Q))
-		isInventoryShowing = false;
+  if (isInventoryShowing) {
+    DrawRectangle(0, 0, GetRenderWidth(), GetRenderHeight(),
+                  Color{10, 10, 10, 218});
+    const std::string inventoryTip =
+        "Drag and drop items to place in your room. press 'Q' to close.";
+    const int inventoryTipSize = MeasureText(inventoryTip.c_str(), 20);
+    const int inventoryTipPos = GetScreenWidth() / 2 - inventoryTipSize / 2;
 
-	if (isInventoryShowing)
-	{
-		DrawRectangle(0, 0, GetRenderWidth(), GetRenderHeight(), Color{ 10,10,10,218 });
-		const std::string inventoryTip = "Drag and drop items to place in your room. press 'Q' to close.";
-		const int inventoryTipSize = MeasureText(inventoryTip.c_str(), 20);
-		const int inventoryTipPos = GetScreenWidth() / 2 - inventoryTipSize / 2;
+    DrawText(inventoryTip.c_str(), inventoryTipPos, GetScreenHeight() - 50, 20,
+             WHITE);
 
-		DrawText(inventoryTip.c_str(), inventoryTipPos, GetScreenHeight() - 50, 20, WHITE);
+    int currentRow = 0;
+    int currentColumn = 0;
 
-		int currentRow = 0;
-		int currentColumn = 0;
+    for (auto it = inventoryVector.begin(); it < inventoryVector.end(); it++) {
+      float posX = startX + currentColumn * spacing;
+      float posY = startY + scrollOffset + currentRow * spacing;
 
-		for (auto it = inventoryVector.begin(); it < inventoryVector.end(); it++)
-		{
-			float posX = startX  + currentColumn * spacing;
-			float posY = startY + scrollOffset +  currentRow * spacing;
+      (*it)->setIsDraggable(true);
+      (*it)->setInventoryPosition({posX, posY});
 
-			(*it)->setIsDraggable(true);
-			(*it)->setInventoryPosition({ posX, posY });
+      DrawTextureEx((*it)->getTexture(), (*it)->getInventoryPosition(), 0, 1,
+                    WHITE);
 
-			DrawTextureEx((*it)->getTexture(), (*it)->getInventoryPosition(), 0, 1, WHITE);
+      currentColumn++;
 
-			currentColumn++;
-
-			if (currentColumn >= maxItemsPerRow)
-			{
-				currentColumn = 0;
-				currentRow++;    
-			}
-		}
-	}
-	else
-	{
-		for (auto& item : inventoryVector)
-			item->setIsDraggable(false);
-	}
+      if (currentColumn >= maxItemsPerRow) {
+        currentColumn = 0;
+        currentRow++;
+      }
+    }
+  } else {
+    for (auto &item : inventoryVector)
+      item->setIsDraggable(false);
+  }
 }
 
-void Player::Inventory::scrollInventoryMenu()
-{
-	const float scrollSpeed = 10.0f;
+void Player::Inventory::scrollInventoryMenu() {
+  const float scrollSpeed = 10.0f;
 
-	if (isInventoryShowing)
-	{
-		if (GetMouseWheelMove() < 0)
-			scrollOffset -= scrollSpeed;
-		if (GetMouseWheelMove() > 0)
-			scrollOffset += scrollSpeed;
-	}
+  if (isInventoryShowing) {
+    if (GetMouseWheelMove() < 0)
+      scrollOffset -= scrollSpeed;
+    if (GetMouseWheelMove() > 0)
+      scrollOffset += scrollSpeed;
+  }
 }
 
-void Player::Inventory::dragItem(std::vector<std::shared_ptr<Entity>>& EntitiesDrawnToWorld)
-{
-	for (size_t i = 0; i < inventoryVector.size(); i++)
-	{
-		auto& item = inventoryVector[i];
-		if (item->getIsBeingDragged())
-		{
-			item->setPosition(GetMousePosition());
-			DrawTextureEx(item->getTexture(), item->getPosition(), 0, 1, WHITE);
-			if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
-			{
-				item->setIsBeingDragged(false);
-				EntitiesDrawnToWorld.push_back(item);
-				inventoryVector.erase(inventoryVector.begin() + i);
-				i--;
-			}
-		}
-		if (CheckCollisionPointRec(GetMousePosition(), {item->getInventoryPositionX(),item->getInventoryPositionY(), static_cast<float>(item->getTextureWidth()), static_cast<float>(item->getTextureHeight())}) 
-			&& IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && item->getIsDraggable()) 
-		{
-			item->setIsBeingDragged(true);
-			isInventoryShowing = false;
-		}
-	}
+void Player::Inventory::dragItem(
+    std::vector<std::shared_ptr<Entity>> &EntitiesDrawnToWorld) {
+  for (size_t i = 0; i < inventoryVector.size(); i++) {
+    auto &item = inventoryVector[i];
+    if (item->getIsBeingDragged()) {
+      item->setPosition(GetMousePosition());
+      DrawTextureEx(item->getTexture(), item->getPosition(), 0, 1, WHITE);
+      if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+        item->setIsBeingDragged(false);
+        EntitiesDrawnToWorld.push_back(item);
+        inventoryVector.erase(inventoryVector.begin() + i);
+        i--;
+      }
+    }
+    if (CheckCollisionPointRec(
+            GetMousePosition(),
+            {item->getInventoryPositionX(), item->getInventoryPositionY(),
+             static_cast<float>(item->getTextureWidth()),
+             static_cast<float>(item->getTextureHeight())}) &&
+        IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && item->getIsDraggable()) {
+      item->setIsBeingDragged(true);
+      isInventoryShowing = false;
+    }
+  }
 }
-
-
