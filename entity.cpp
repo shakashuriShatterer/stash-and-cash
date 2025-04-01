514350afb304f25c128cbcf1986197cec57509b4
shakashuriShatterer <oeagle@protonmail.com>
2025-04-01 22:00:21 +0000

    moved Ganja class to seperate file

--- entity.cpp
+++ entity.cpp
@@ -107,39 +107,4 @@ bool Entity::getIsInteractionMenuShowing() const {
 
 void Entity::interactionMenu() {}
 
-const std::chrono::steady_clock::time_point Ganja::getTimePlanted() const {
-  return timePlanted;
-}
-const std::chrono::steady_clock::time_point Ganja::getTimePlantedNew() const {
-  return timePlantedNew;
-}
-const std::chrono::duration<double> Ganja::getTimeElapsed() const {
-  return timePlantedElapsed;
-}
 
-Ganja::Ganja() : Entity("weed") {
-  setTexture(LoadTexture("ganja.png"));
-  setInventoryPosition({10, 10});
-  setTextureSize(50, 50);
-  growthCount = 0;
-  timePlanted = std::chrono::steady_clock::now();
-}
-
-void Ganja::interactionMenu() {
-  if (getIsInteractionMenuShowing())
-    DrawRectangle(0, 0, GetRenderWidth(), GetRenderHeight(),
-                  Color{10, 10, 10, 218});
-}
-
-void Ganja::grow() {
-  timePlantedNew = std::chrono::steady_clock::now();
-  timePlantedElapsed =
-      std::chrono::duration_cast<std::chrono::duration<double>>(timePlantedNew -
-                                                                timePlanted);
-
-  if (timePlantedElapsed.count() > 5) {
-    timePlanted = std::chrono::steady_clock::now();
-    setTextureSize(getTextureWidth() + 20, getTextureHeight() + 20);
-    setPosition({getPositionX() - 20, getPositionY() - 40});
-  }
-}
--- entity.h
+++ entity.h
@@ -1,5 +1,5 @@
-#pragma once
 #include "pch.h"
+#pragma once
 
 struct IdManager {
   static int nextId;
@@ -50,25 +50,4 @@ public:
   bool getIsInteractionMenuShowing() const;
 
   virtual void interactionMenu();
-};
-
-class Ganja : public Entity {
-private:
-  std::chrono::steady_clock::time_point timePlanted, timePlantedNew;
-  std::chrono::duration<double> timePlantedElapsed;
-  int growthCount;
-
-public:
-  Ganja();
-  void update() override { grow(); }
-  void interactionMenu() override;
-  void grow();
-  // setters
-  void setTimePlanted(const std::chrono::steady_clock &);
-  void setTimeNew(const std::chrono::steady_clock &);
-
-  // getters
-  const std::chrono::steady_clock::time_point getTimePlanted() const;
-  const std::chrono::steady_clock::time_point getTimePlantedNew() const;
-  const std::chrono::duration<double> getTimeElapsed() const;
 };
